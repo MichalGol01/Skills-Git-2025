5dc725da76a26f01b0e1f50867bb57090bc46687
MichalGol01 <goldyn.m01@student.lmas.ac.uk>
2025-03-27 11:23:30 +0000

    slide 15

---
+++ NEA SKILLS BUILDING 2025/Assets/Scripts/DinoAnimation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DinoAnimation : MonoBehaviour {
+
+	Animator MyAnimator;
+
+
+	void Start () {
+		MyAnimator = GetComponent<Animator> ();
+	}
+	
+
+
+
+	void Update () {
+
+		if (Input.GetKey (KeyCode.RightArrow)) 
+		{
+			MyAnimator.SetBool ("walk", true);
+		}
+		else if (Input.GetKey (KeyCode.DownArrow)) 
+		{
+			MyAnimator.SetBool ("dead", true);
+		}
+		else if (Input.GetKey (KeyCode.R)) 
+		{
+			MyAnimator.SetBool ("run", true);
+		}
+		else if (MyAnimator.GetBool("dead")==false) 
+		{
+			MyAnimator.SetBool ("idle", true);
+			MyAnimator.SetBool ("walk", false);
+			MyAnimator.SetBool ("run", false);
+		}
+
+	}
+}
