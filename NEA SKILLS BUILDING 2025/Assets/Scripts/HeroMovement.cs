59966126c838cacd1ec6f24f5b5088713f8ec823
MichalGol01 <goldyn.m01@student.lmas.ac.uk>
2025-03-28 16:04:19 +0000

    End of Task 2

---
+++ NEA SKILLS BUILDING 2025/Assets/Scripts/HeroMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroMovement : MonoBehaviour {
+
+	Rigidbody2D rb;
+	float speed;
+
+
+
+	void resetPosition() {
+		Debug.Log ("Spiked Recived!");
+		transform.SetPositionAndRotation (new Vector3 (-5.49f, 0.8f, 0), Quaternion.identity);
+	}
+
+	void Start () {
+		speed = 5.1f;
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Translate (Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0, 0);
+		}
+
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			rb.AddForce (Vector2.up * 300);
+		}
+	}
+}
