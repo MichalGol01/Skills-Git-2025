2e4b5604669489b0ed17cd375dcd1a1641632722
MichalGol01 <goldyn.m01@student.lmas.ac.uk>
2025-04-24 09:58:16 +0100

    End Of Task 5

--- NEA SKILLS BUILDING 2025/Assets/Scripts/Altar.cs
+++ NEA SKILLS BUILDING 2025/Assets/Scripts/Altar.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Altar : MonoBehaviour {
 
 	private void OnCollisionEnter2D (Collision2D other) {
-		
+		if (other.gameObject.tag == "Player") {
+			SceneManager.LoadScene("Won");
+		}
 	}
 
 	// Use this for initialization
