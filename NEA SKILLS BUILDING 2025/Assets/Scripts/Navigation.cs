ca696f9fbcb3e704b4956b762ecc3f616794765d
MichalGol01 <goldyn.m01@student.lmas.ac.uk>
2025-04-24 10:55:12 +0100

    End Of Task 6

---
+++ NEA SKILLS BUILDING 2025/Assets/Scripts/Navigation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Navigation : MonoBehaviour {
+
+	private Canvas mainCanvas;
+	public TextMeshPro timer;
+
+	public void ClickedButtonA(){
+
+		Debug.Log("Button A Clicked!");
+	}
+	public void ClickedButtonB(){
+
+		Debug.Log("Button B Clicked!");
+	}
+	public void ClickedButtonExit(){
+
+		Debug.Log("Exit Button Clicked!");
+		Time.timeScale = 1;
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		mainCanvas = GameObject.FindObjectOfType<Canvas> ();
+		mainCanvas.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer.text = Time.fixedTime.ToString ("0.00");
+
+		if (Input.GetKey (KeyCode.Escape)) {
+
+			mainCanvas.enabled = true;
+			Time.timeScale = 0f;
+		}
+			
+	}
+}
