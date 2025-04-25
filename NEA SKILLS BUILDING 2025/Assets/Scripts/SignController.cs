92e097c62a8d9ede29ab6a699a7aced41ab6641e
MichalGol01 <goldyn.m01@student.lmas.ac.uk>
2025-04-25 16:27:24 +0100

    slide 106

---
+++ NEA SKILLS BUILDING 2025/Assets/Scripts/SignController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignController : MonoBehaviour {
+
+	private void OnTriggerEnter2D(Collider2D other) {
+
+		Debug.Log ("The sign has felt your presence!");
+	}
+
+	private void OnTriggerExit2D(Collider2D other) {
+
+		Debug.Log ("The sign can no longer feel your presence! Come Back!");
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
