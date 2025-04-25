38d2fad02998b4ce761f66585b8a23d8e9036e83
MichalGol01 <goldyn.m01@student.lmas.ac.uk>
2025-04-25 15:00:14 +0100

    End Of Task 8

---
+++ NEA SKILLS BUILDING 2025/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+	public AudioClip coin;
+	public AudioClip bomb;
+	public AudioClip life;
+
+	private AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+		source = GetComponent<AudioSource> ();
+	}
+
+	private void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.tag == "extraLife") {
+
+			source.PlayOneShot (life, 1);
+		} 
+		else if (other.gameObject.tag == "bomb") {
+			
+			source.PlayOneShot (bomb, 1);
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) {
+
+		Debug.Log ("bing!");
+		source.PlayOneShot (coin, 1);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+}
