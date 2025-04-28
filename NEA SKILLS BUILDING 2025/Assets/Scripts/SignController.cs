a85e5dafd8836123803e28deba6ca9c4f60dad02
MichalGol01 <goldyn.m01@student.lmas.ac.uk>
2025-04-28 13:49:05 +0100

    End Of Task 11

--- NEA SKILLS BUILDING 2025/Assets/Scripts/SignController.cs
+++ NEA SKILLS BUILDING 2025/Assets/Scripts/SignController.cs
@@ -1,21 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SignController : MonoBehaviour {
 
+	public TextMeshPro signText;
+
 	private void OnTriggerEnter2D(Collider2D other) {
 
+		// will run once when you enter the trigger
 		Debug.Log ("The sign has felt your presence!");
+		signText.text = "The sign has felt your presence";
 	}
 
 	private void OnTriggerExit2D(Collider2D other) {
 
+		//run once you left the trigger
 		Debug.Log ("The sign can no longer feel your presence! Come Back!");
+	}
+	private void OnTriggerStay2D(Collider2D other) {
+
+		//runs each time you move inside the trigger
+		Debug.Log("Why are you still here? Go away!");
+		signText.text = "The sign can no longer feel your presence Come back!";
 	}
 	// Use this for initialization
 	void Start () {
-		
+		signText = GetComponentInChildren<TextMeshPro> ();
 	}
 	
 	// Update is called once per frame
